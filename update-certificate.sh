de84d9e7c2ff17a31484740cb84c4df2a2fe6fb9
torben <t.sickert[~at~]gmail.com>
2022-02-22 19:57:08 +0100

    Automatic update to new version.

--- update-certificate.sh
+++ update-certificate.sh
@@ -5,12 +5,12 @@ set -e
 echo Update certificate for \"$1\".
 
 # Ensure presence of needed acme challenge locations.
-mkdir --parents "${APPLIATION_PATH}/certificates/acme-challenge"
+mkdir --parents "${APPLIATION_PATH}certificates/acme-challenge"
 mkdir --parents "${2}letsEncrypt/.well-known"
 ln \
     --force \
     --symbolic \
-    "${APPLIATION_PATH}/certificates/acme-challenge" \
+    "${APPLIATION_PATH}certificates/acme-challenge" \
     "${2}letsEncrypt/.well-known/acme-challenge"
 
 certbot renew \
